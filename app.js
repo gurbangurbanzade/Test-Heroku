94486909eafb74fadf7b702204bbe333882ba560
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-08-20 20:17:09 +0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { check, validationResult } = require("express-validator");
 const app = express();
-const port = process.envPORT || 5000;
+const port = process.env.PORT || 5000;
 
 // Set Templating Enginge
 app.set("view engine", "ejs");
