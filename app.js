6e4e60d60da39262ddb56bc304fa3b1320566dd0
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-08-20 13:36:51 +0400

    first online app

---
+++ app.js
@@ -0,0 +1,26 @@
+//Imports
+const express = require("express");
+const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
+const app = express();
+const port = 5000;
+
+// Set Templating Enginge
+app.set("view engine", "ejs");
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// Navigation
+
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.post("/register", urlencodedParser, (req, res) => {
+  res.json(req.body);
+});
+
+// Listen on Port 5000
+app.listen(port, () => console.info(`App listening on port ${port}`));
